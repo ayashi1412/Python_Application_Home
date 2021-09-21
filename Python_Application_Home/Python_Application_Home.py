ce985f4c92bd6b97a2b58b8a0437645afdb7b98a
Binod <ilikeit_12@hotmail.com>
2021-09-21 17:23:51 +0100

    Module 2.6.1.9 to 2.6.1.11

--- Python_Application_Home/Python_Application_Home.py
+++ Python_Application_Home/Python_Application_Home.py
@@ -1,44 +1,31 @@
-#MODULE2.2.1.11 LAB Python Liberals
+#SSgt Binod Gurung, Codes for Chapter 2.6.1.9 to 2.6.1.11.  Date: 21 Sept 2021
 
-print('\"I\'m"\n\""learning""\n\"""Python"""')
+#MODULE 2.6.1.9 LAB: Simple Input and Output. Evaluating the results of 4 arithmetic operations.
+ 
+a = float(input("Enter the first float value: ")) # input a float value for variable a here
+b = float(input("Enter the second float value: "))# input a float value for variable b here
 
-#MODULE2.4.1.7 LAB Variables
+print("The sum of the float values is: " + str(a + b)) # output the result of addition here
+print("The difference of the float values is: " + str(a - b)) # output the result of subtraction here
+print("The product of the float values is: " + str(a * b)) # output the result of multiplication here
+print("The Quotient of the float values is: " + str(a / b)) # output the result of division here
+print("\nThat's all, folks!") #\n will start a newline after the print output.
 
-John = 3
-Mary = 5
-Adam = 6
-print(John , Mary , Adam, sep=",")
+#MODULE 2.6.1.10 LAB: Operators and expressions. Evaluate an algebric expression
 
-total_apples = John + Mary + Adam
-
-print(total_apples)
-
-#MODULE2.4.1.9 LAB Variables: A Simple Converter
-
-kilometers = 12.25
-miles = 7.38
-
-miles_to_kilometers = miles * 1.61
-kilometers_to_miles = kilometers / 1.61
-
-print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
-print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
-
-#MODULE2.4.10 LAB: Operators and expressions
-
-x =  0
-x = float(x)
-y = (3 * x ** 3) - (2 * x ** 2) + (3 * x) - 1
+x = float(input("Enter value for x: ")) #Input user's float value.
+y = 1 / (x + (1/(x + (1/(x + (1/x)))))) # 1/x is represenation of 1/x in algrbric expression.
 print("y =", y)
 
-#MODULE2.5.1.2 LAB: Comments
+#MODULE 2.6.1.11 LAB: Operators and expressions. Evaluate the end time of a period of time.
 
-#this program computes the number of seconds in a given number of hours
+hour = int(input("Starting time (hours): "))    #Input starting Time in hours.
+mins = int(input("Starting time (minutes): "))  #Input starting Time in Minutes.
+dura = int(input("Event duration (minutes): ")) #Input the event duration in Minutes.
 
-a = 2 # number of hours
-seconds = 3600 # number of seconds in 1 hour
+Total_Minutes = (mins + dura)    #Calculates the total time spent in minutes.
+End_Minutes = Total_Minutes % 60 #% 60 calculates the remainder of the minutes.
+hr = hour + (Total_Minutes//60)  #//60 calculates the hour carried over.
+End_hr = hr % 24                 #%24 calculates the 24 hr clock when the hr goes beyound 24.
 
-print("Hours: ", a) #printing the number of hours
-print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
-
-#this is the end of the program that computes the number of seconds in 3 hour
+print("The End time is ",End_hr,":",End_Minutes)
